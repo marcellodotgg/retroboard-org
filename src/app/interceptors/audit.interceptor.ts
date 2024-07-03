bbec80bc5f06043211b8d2d7f8df79493cced016
Marcello <marcello@bytebury.com>
2024-07-03 15:00:41 -0400

    feature: create audit service

--- src/app/interceptors/audit.interceptor.ts
+++ src/app/interceptors/audit.interceptor.ts
@@ -1,9 +1,12 @@
 import { HttpEvent, HttpHandlerFn, HttpRequest } from '@angular/common/http';
+import { inject } from '@angular/core';
 import { Observable } from 'rxjs';
+import { AuditService } from '../services/audit.service';
 
 export function auditInterceptor(request: HttpRequest<unknown>, next: HttpHandlerFn): Observable<HttpEvent<unknown>> {
+  const auditId = inject(AuditService).whoAmI();
   const requestWithAuditHeader = request.clone({
-    headers: request.headers.append('X-retro-audit-id', 'test'),
+    headers: request.headers.append('X-retro-audit-id', auditId),
   });
   return next(requestWithAuditHeader);
 }
---
+++ src/app/services/audit.service.ts
@@ -0,0 +1,26 @@
+import { isPlatformServer } from '@angular/common';
+import { inject, Injectable, PLATFORM_ID } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuditService {
+  private readonly platformId = inject(PLATFORM_ID);
+
+  whoAmI(): string {
+    if (isPlatformServer(this.platformId)) return '';
+
+    const auditId = localStorage.getItem('audit_id');
+
+    if (!auditId) {
+      return this.createAuditId();
+    }
+    return auditId;
+  }
+
+  private createAuditId(): string {
+    const uuid = crypto.randomUUID();
+    localStorage.setItem('audit_id', uuid);
+    return uuid;
+  }
+}
