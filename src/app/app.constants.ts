b56ab06b5fce6e6cf122b99af110da53db9b53cf
Marcello <marcello@bytebury.com>
2024-07-03 14:23:11 -0400

    chore: update the api endpoint

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -1,7 +1,7 @@
 import { isDevMode } from '@angular/core';
 
 export const GITHUB_REPO = 'https://github.com/bytebury/retroboard-org';
-export const API_ROOT = isDevMode() ? 'http://localhost:8080' : 'http://api.bytebury.com:8081';
+export const API_ROOT = isDevMode() ? 'http://localhost:8080' : 'https://retro.bytebury.com';
 
 export const DEFAULT_BOARD_CONFIG = {
   name: 'My Retro',
