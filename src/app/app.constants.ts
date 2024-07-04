399734ad0021279c7644de5e67c83980a81b5ea9
Marcello <marcello@bytebury.com>
2024-07-04 02:23:37 -0400

    fix: websocket URL

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -3,7 +3,7 @@ import packageInfo from '../../package.json';
 
 export const GITHUB_REPO = 'https://github.com/bytebury/retroboard-org';
 export const API_ROOT = isDevMode() ? 'http://localhost:8080' : 'https://retro.bytebury.com';
-export const API_WEBSOCKET_URL = isDevMode() ? 'ws://localhost:8080/ws' : 'wss://retro.bytebury.com/ws';
+export const API_WEBSOCKET_URL = isDevMode() ? 'ws://localhost:8080/ws' : 'ws://retro.bytebury.com/ws';
 export const APP_VERSION = packageInfo.version;
 
 export const DEFAULT_BOARD_CONFIG = {
