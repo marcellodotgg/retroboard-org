386e1ba0253edc69cdc6b8115f922c9e5cc0a901
Marcello <marcello@bytebury.com>
2024-07-06 14:01:51 -0400

    feature: update title for privacy page

--- src/app/pages/privacy-page/privacy-page.component.ts
+++ src/app/pages/privacy-page/privacy-page.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-privacy-page',
@@ -9,4 +10,8 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   styleUrl: './privacy-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class PrivacyPageComponent {}
+export class PrivacyPageComponent {
+  constructor(private readonly title: Title) {
+    this.title.setTitle('Retroboard | Privacy Policy');
+  }
+}
